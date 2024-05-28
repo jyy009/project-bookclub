df624532950b2b8184e4541eddb6cb3aee102fc7
Teddy <yoo.ji.yg@gmail.com>
2024-05-28 19:57:42 +0200

    refactor server.js

---
+++ backend/models/User.js
@@ -0,0 +1,38 @@
+import mongoose from "../config/database.js";
+
+const { Schema, model } = mongoose;
+
+// Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => bcrypt.genSaltSync(),
+  },
+});
+
+// Model
+const User = model("User", userSchema);
+
+export default User
