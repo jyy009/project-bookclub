e1b72a126632c545eacce05176eae53a3de83360
Viktoria Olofsson <viktoriaolofsson@gmail.com>
2024-06-06 12:21:00 +0200

    not found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import { useUserStore } from "../store/useUserStore";
+import { Headline } from "../atoms/Headline";
+import { Image } from "../atoms/Image";
+import { Text } from "../atoms/Text";
+import { Link } from "react-router-dom";
+import { Button } from "../atoms/Button";
 
 export const NotFound = () => {
-  return <div>NotFound</div>;
+  const { isLoggedIn } = useUserStore();
+  console.log(isLoggedIn);
+
+  return (
+    <section className="h-full">
+      <div className="flex flex-col items-center mx-4 md:mx-8 lg:mx-32 py-7 md:py-10 lg:py-36 gap-6 md:gap-8">
+        <Headline titleText={"The page you were looking for doesn't exist."} />
+        <Image
+          link={"/images/empty_book.jpg"}
+          imgText={"Empty book"}
+          section={"object-cover w-full md:max-h-96 md:max-w-4xl rounded-lg"}
+        />
+        {!isLoggedIn ? (
+          <Text text={"Please sign in and try again."} />
+        ) : (
+          <Text text={"Please try again."} />
+        )}
+
+        <Link to={"/"}>
+          <Button
+            btnText={"Back to Home"}
+            buttonStyle={
+              "bg-tertiary px-8 py-2 text-secondary font-josefinsans rounded-md"
+            }
+          />
+        </Link>
+      </div>
+    </section>
+  );
 };
