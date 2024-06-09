5c9f7ac29e6fbc9989343fc9c328f500b007e93f
schouenkes <m.schouenke@gmail.com>
2024-06-09 19:00:44 +0200

    hid the sign in form when the loading animation shows

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -31,42 +31,43 @@ export const SignIn = () => {
           let the literary adventures continue!
         </p>
       </div>
-      {isLoading ? <Loading /> : ""}
-      <div>
-        <TextInput
-          label={"Username"}
-          inputType={"text"}
-          inputName={"username"}
-          placeholder={"Type your username"}
-          value={loginData.username}
-          onChange={(event) =>
-            handleLoginChange("username", event.target.value)
-          }
-          labelStyle="font-josefinsans text-base flex flex-col mx-3 my-2"
-          inputStyle="font-worksans text-sm border-2 rounded-lg p-1"
-        />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <div>
+            <TextInput
+              label={"Username"}
+              inputType={"text"}
+              inputName={"username"}
+              placeholder={"Type your username"}
+              value={loginData.username}
+              onChange={(event) => handleLoginChange("username", event.target.value)}
+              labelStyle="font-josefinsans text-base flex flex-col mx-3 my-2"
+              inputStyle="font-worksans text-sm border-2 rounded-lg p-1"
+            />
 
-        <TextInput
-          label={"Password"}
-          inputType={"password"}
-          inputName={"password"}
-          placeholder={"Type your password"}
-          value={loginData.password}
-          onChange={(event) =>
-            handleLoginChange("password", event.target.value)
-          }
-          labelStyle="font-josefinsans text-base flex flex-col mx-3 my-2"
-          inputStyle="font-worksans text-sm border-2 rounded-lg p-1"
-        />
-      </div>
-      <div className="flex justify-end mr-3 mt-3">
-        <Button
-          type={"submit"}
-          btnText={"Sign in"}
-          disabled={isLoading}
-          buttonStyle="font-josefinsans bg-jeans text-cream px-6 py-1 flex rounded lg"
-        />
-      </div>
+            <TextInput
+              label={"Password"}
+              inputType={"password"}
+              inputName={"password"}
+              placeholder={"Type your password"}
+              value={loginData.password}
+              onChange={(event) => handleLoginChange("password", event.target.value)}
+              labelStyle="font-josefinsans text-base flex flex-col mx-3 my-2"
+              inputStyle="font-worksans text-sm border-2 rounded-lg p-1"
+            />
+          </div>
+          <div className="flex justify-end mr-3 mt-3">
+            <Button
+              type={"submit"}
+              btnText={"Sign in"}
+              disabled={isLoading}
+              buttonStyle="font-josefinsans bg-jeans text-cream px-6 py-1 flex rounded lg"
+            />
+          </div>
+        </>
+      )}
     </form>
   );
 };
