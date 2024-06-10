7550e62fbe593bd7fe219ef9567c9879fe4e30f3
schouenkes <m.schouenke@gmail.com>
2024-06-10 10:40:20 +0200

    fixed a bug in wishlist

--- frontend/src/pages/Wishlist.jsx
+++ frontend/src/pages/Wishlist.jsx
@@ -28,47 +28,36 @@ export const Wishlist = () => {
   return (
     <div className={"flex flex-col items-center"}>
       {isLoggedIn ? (
-
-         <div className="flex flex-col border justify-center items-center">
+        <>
+          <div className="flex flex-col border justify-center items-center">
             <Headline section={"py-0 pt-4"} titleText={"Book Wishlist"} />
             <Text section={"pb-2"} text={"Add your book wish!"} />
           </div>
           <WishlistForm />
-          <WishlistNav
-            sortWishes={sortWishes}
-            navigatePage={navigatePage}
-            currentPage={currentPage}
-          />
-              <div className="max-w-md mx-auto p-2">
-          {isLastPage && (
-            <div className="text-center p-12">
-              <Text
-                text={
-                  "There are no more books on the wishlist. Why don't you add one? 😊"
-                }
-              />
-            </div>
-          )}
-          {!isLastPage &&
-            wishlist.map((wish) => (
-              <WishlistCard
-                key={wish._id}
-                id={wish._id}
-                title={wish.title}
-                author={wish.author}
-                message={wish.message}
-                user={wish.user}
-                likes={wish.likes}
-              />
-            ))}
-            </div>
-        </div>
+          <WishlistNav sortWishes={sortWishes} navigatePage={navigatePage} currentPage={currentPage} />
+          <div className="max-w-md mx-auto p-2">
+            {isLastPage && (
+              <div className="text-center p-12">
+                <Text text={"There are no more books on the wishlist. Why don't you add one? 😊"} />
+              </div>
+            )}
+            {!isLastPage &&
+              wishlist.map((wish) => (
+                <WishlistCard
+                  key={wish._id}
+                  id={wish._id}
+                  title={wish.title}
+                  author={wish.author}
+                  message={wish.message}
+                  user={wish.user}
+                  likes={wish.likes}
+                />
+              ))}
+          </div>
+        </>
       ) : (
         <Navigate replace to="/sign-up" /> // This doesn't appear to work. Navigates to "/"
       )}
-
-</div>
-
-
+    </div>
   );
 };
