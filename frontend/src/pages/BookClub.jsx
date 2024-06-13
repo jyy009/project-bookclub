e354a313593823678526fccfdad28cda33b3cc11
Viktoria Olofsson <viktoriaolofsson@gmail.com>
2024-06-13 19:37:33 +0200

    updated book info

--- frontend/src/pages/BookClub.jsx
+++ frontend/src/pages/BookClub.jsx
@@ -10,10 +10,26 @@ export const BookClub = () => {
   return (
     <section className="flex flex-col gap-6 lg:gap-10 py-12">
       <InfoCard
-        imgText={"Books and magazines on a sheet"}
-        imgSrc={"/images/giulia-bertelli-y7rGTFyOzxc-unsplash.jpg"}
-        titleText={isLoggedIn ? loggedInInfo.sectionOne.heading : loggedOutInfo.sectionOne.heading}
-        text={isLoggedIn ? loggedInInfo.sectionOne.text : loggedOutInfo.sectionOne.text}
+        imgText={
+          isLoggedIn
+            ? loggedInInfo.sectionOne.imageText
+            : loggedOutInfo.sectionOne.imageText
+        }
+        imgSrc={
+          isLoggedIn
+            ? loggedInInfo.sectionOne.imgSrc
+            : loggedOutInfo.sectionOne.imgSrc
+        }
+        titleText={
+          isLoggedIn
+            ? loggedInInfo.sectionOne.heading
+            : loggedOutInfo.sectionOne.heading
+        }
+        text={
+          isLoggedIn
+            ? loggedInInfo.sectionOne.text
+            : loggedOutInfo.sectionOne.text
+        }
         button={isLoggedIn ? false : true}
         mdScreen={"md:flex-row"}
         btnText={"Sign Up"}
@@ -21,17 +37,37 @@ export const BookClub = () => {
       />
       <InfoCard
         imgText={"Person sitting on top of a stack of book"}
-        imgSrc={"/images/pexels-merve-bayar-158520570-10863551.jpg"}
-        titleText={isLoggedIn ? loggedInInfo.sectionTwo.heading : loggedOutInfo.sectionTwo.heading}
-        text={isLoggedIn ? loggedInInfo.sectionTwo.text : loggedOutInfo.sectionTwo.text}
+        imgSrc={
+          isLoggedIn
+            ? loggedInInfo.sectionTwo.imgSrc
+            : loggedOutInfo.sectionTwo.imgSrc
+        }
+        titleText={
+          isLoggedIn
+            ? loggedInInfo.sectionTwo.heading
+            : loggedOutInfo.sectionTwo.heading
+        }
+        text={
+          isLoggedIn
+            ? loggedInInfo.sectionTwo.text
+            : loggedOutInfo.sectionTwo.text
+        }
         button={false}
         mdScreen={"md:flex-row-reverse"}
       />
       <InfoCard
         imgText={"Person holding an open book, confetti coming out of the book"}
         imgSrc={"/images/woman-6318447_640.jpg"}
-        titleText={isLoggedIn ? loggedInInfo.sectionThree.heading : loggedOutInfo.sectionThree.heading}
-        text={isLoggedIn ? loggedInInfo.sectionThree.text : loggedOutInfo.sectionThree.text}
+        titleText={
+          isLoggedIn
+            ? loggedInInfo.sectionThree.heading
+            : loggedOutInfo.sectionThree.heading
+        }
+        text={
+          isLoggedIn
+            ? loggedInInfo.sectionThree.text
+            : loggedOutInfo.sectionThree.text
+        }
         button={isLoggedIn ? true : false}
         mdScreen={"md:flex-row"}
         btnText={"Wishlist"}
