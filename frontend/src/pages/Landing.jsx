cf1c6c3b69de18de5db4aaa481b0d68d4958a258
schouenkes <m.schouenke@gmail.com>
2024-06-13 13:21:00 +0200

    changed the landing page text when signed in

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -7,35 +7,43 @@ import { useUserStore } from "../store/useUserStore";
 
 export const Landing = () => {
   const { isLoggedIn } = useUserStore();
+
   return (
     <div className="bg-secondary flex justify-center items-center">
-        <div className="bg-secondary flex flex-col py-8 md:grid md:grid-cols-2 md:px-8  md:max-w-screen-md lg:w-fit">
-            <Image
-            link={"/images/landing-image.jpg"}
-            imgText={"girl reading book"}
-          />
-          
-          <div className="flex flex-col md:col-span-1 gap-6  md:px-8 text-center">
-            <Headline titleText={"Discover your next favorite book with us"} />
-            <Text
-              text={
-                "Whether you’re a fan of gripping thrillers, heartwarming romances, or thought-provoking non-fiction, our book club offers something for everyone. Connect with fellow book lovers, engage in lively discussions, and discover new favourites each month. "
-              }
-            />
-          {!isLoggedIn && (
-            <Link to="/sign-up">
-              <Button
-                btnText={"Sign up"}
-                buttonStyle={
-                  "bg-tertiary px-12 py-2 text-secondary font-josefinsans md:text-xl rounded-md"
+      <div className="bg-secondary flex flex-col py-8 md:grid md:grid-cols-2 md:px-8  md:max-w-screen-md lg:w-fit">
+        <Image link={"/images/landing-image.jpg"} imgText={"girl reading book"} />
+
+        <div className="flex flex-col md:col-span-1 gap-6  md:px-8 text-center">
+          <Headline titleText={"Discover your next favorite book with us"} />
+          {!isLoggedIn ? (
+            <>
+              <Text
+                text={
+                  "Whether you’re a fan of gripping thrillers, heartwarming romances, or thought-provoking non-fiction, our book club offers something for everyone. Connect with fellow book lovers, engage in lively discussions, and discover new favourites each month. "
                 }
               />
-            </Link>
+              <div className="flex justify-center mt-4">
+                <Link to="/sign-up">
+                  <Button btnText={"Sign up"} />
+                </Link>
+              </div>{" "}
+            </>
+          ) : (
+            <>
+              <Text text={`Have you found a book you want to read and share with others?`} />
+              <Text text={`Share it in the Wishlist!`} />
+              <Text text={`It can be a timeless classic, an exciting new release or a hidden gem of any genre.`} />
+              <div className="flex justify-center mt-4">
+                <Link to="/wishlist">
+                  <Button btnText={"Wishlist"} />
+                </Link>
+              </div>{" "}
+            </>
           )}
-            
-          </div>
         </div>
-        </div>
-      )}
+      </div>
+    </div>
+  );
+};
 
-
+("");
